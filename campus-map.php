d017d945f39d494c33abaaedb9f7b121d8e4aad2
user <maxifalcone@gmail.com>
2016-07-15 12:17:38 -0300

    map

--- campus-map.php
+++ campus-map.php
@@ -183,6 +183,124 @@
                 }    ,
 
 
+            //------------------------------------------
+  
+            
+                {
+                    //==================================
+                    // Basic settings
+                    //==================================
+                    name        : '5',
+                    naming_opp  : 'cafeteria-building/b.bookstore.html',                    
+                    external_link: 'dining-hall.php',
+                    pointer_pos : [212.3, 558.6],
+                    sceneFunc: function(ctx) 
+                    {
+                        ctx.moveTo(127.1, 454.6);
+                        ctx.lineTo(208.3, 435.6);
+                        ctx.lineTo(210.8, 439.6);
+                        ctx.lineTo(231.5, 434.0);
+                        ctx.lineTo(233.0, 436.1);
+                        ctx.lineTo(253.8, 430.6);
+                        ctx.lineTo(262.5, 442.5);
+                        ctx.lineTo(271.1, 444.3);
+                        ctx.lineTo(279.6, 447.6);
+                        ctx.lineTo(303.0, 440.6);
+                        ctx.lineTo(309.8, 448.1);
+                        ctx.lineTo(310.8, 454.6);
+                        ctx.lineTo(336.0, 486.5);
+                        ctx.lineTo(349.1, 496.6);
+                        ctx.lineTo(350.0, 502.0);
+                        ctx.lineTo(360.5, 507.8);
+                        ctx.lineTo(361.5, 516.6);
+                        ctx.lineTo(372.1, 521.6);
+                        ctx.lineTo(372.8, 531.1);
+                        ctx.lineTo(383.8, 536.8);
+                        ctx.lineTo(384.5, 547.1);
+                        ctx.lineTo(396.1, 551.8);
+                        ctx.lineTo(397.5, 563.1);
+                        ctx.lineTo(409.3, 569.0);
+                        ctx.lineTo(410.5, 579.5);
+                        ctx.lineTo(424.1, 585.8);
+                        ctx.lineTo(424.5, 596.3);
+                        ctx.lineTo(438.1, 602.6);
+                        ctx.lineTo(439.3, 614.8);
+                        ctx.lineTo(453.8, 621.8);
+                        ctx.lineTo(453.5, 633.6);
+                        ctx.lineTo(469.6, 641.8);
+                        ctx.lineTo(469.3, 646.6);
+                        ctx.lineTo(473.1, 650.6);
+                        ctx.lineTo(478.5, 677.3);
+                        ctx.lineTo(450.0, 687.5);
+                        ctx.lineTo(449.6, 690.3);
+                        ctx.lineTo(416.1, 702.1);
+                        ctx.lineTo(412.2, 698.6);
+                        ctx.lineTo(249.6, 755.0);
+                        ctx.lineTo(250.8, 760.3);
+                        ctx.lineTo(209.6, 774.1);
+                        ctx.lineTo(207.6, 772.0);
+                        ctx.lineTo(173.6, 783.8);
+                        ctx.lineTo(166.8, 768.1);
+                        ctx.lineTo(162.5, 754.5);
+                        ctx.lineTo(148.0, 759.8);
+                        ctx.lineTo(145.0, 748.0);
+                        ctx.lineTo(157.1, 731.1);
+                        ctx.lineTo(156.1, 729.0);
+                        ctx.lineTo(138.8, 735.6);
+                        ctx.lineTo(135.5, 725.0);
+                        ctx.lineTo(147.3, 708.1);
+                        ctx.lineTo(146.5, 706.6);
+                        ctx.lineTo(130.1, 712.3);
+                        ctx.lineTo(126.8, 701.8);
+                        ctx.lineTo(138.1, 685.1);
+                        ctx.lineTo(137.1, 684.1);
+                        ctx.lineTo(120.6, 690.1);
+                        ctx.lineTo(117.5, 680.6);
+                        ctx.lineTo(128.8, 665.3);
+                        ctx.lineTo(127.5, 663.6);
+                        ctx.lineTo(112.0, 668.8);
+                        ctx.lineTo(109.5, 659.0);
+                        ctx.lineTo(119.0, 643.8);
+                        ctx.lineTo(104.5, 648.8);
+                        ctx.lineTo(101.1, 638.6);
+                        ctx.lineTo(110.5, 626.1);
+                        ctx.lineTo(109.6, 625.0);
+                        ctx.lineTo(97.1, 629.5);
+                        ctx.lineTo(93.8, 619.8);
+                        ctx.lineTo(102.6, 608.3);
+                        ctx.lineTo(102.0, 606.3);
+                        ctx.lineTo(89.3, 610.6);
+                        ctx.lineTo(86.8, 600.6);
+                        ctx.lineTo(94.8, 591.1);
+                        ctx.lineTo(82.5, 592.6);
+                        ctx.lineTo(79.0, 583.3);
+                        ctx.lineTo(87.0, 572.6);
+                        ctx.lineTo(86.0, 572.1);
+                        ctx.lineTo(75.5, 575.5);
+                        ctx.lineTo(72.3, 566.3);
+                        ctx.lineTo(84.1, 551.6);
+                        ctx.lineTo(75.8, 529.3);
+                        ctx.lineTo(92.1, 505.6);
+                        ctx.lineTo(80.6, 474.1);
+                        ctx.lineTo(106.1, 468.1);
+                        ctx.lineTo(105.3, 462.6);
+                        ctx.lineTo(126.8, 457.8);
+                        ctx.lineTo(127.1, 454.6);
+                    },
+                    fill        : 'rgba(170,255,0,0.3)',
+                    stroke      : '#456800',
+                    strokeWidth : 1,     
+
+                    //==================================
+                    // Tooltip settings
+                    //==================================
+                    tooltip     : {                
+                        text: 'Cafeteria',
+                        fontSize: 14,                         
+                        align: 'center'            
+                    }        
+                }    ,
+
 
             ];
 
